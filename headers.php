53c2c855315e3dc75b367e84926fd89d67ca92cd
Ademola Afolabi <afolabiademola27@gmail.com>
2025-08-18 13:09:57 +0100

    feat(headers): added dynamic CORS

--- headers.php
+++ headers.php
@@ -1,5 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:4200');
+$allowedOrigins = ['http://localhost:4200', 'https://jobnet.vercel.app'];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
